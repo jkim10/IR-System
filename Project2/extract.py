5dbf93d3c3f008388162359305dfdae28d09a4ca
jkim10 <jyk2149@columbia.edu>
2021-03-11 01:17:05 -0500

    soup

--- Project2/extract.py
+++ Project2/extract.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
+import urllib
 import requests
-
 from bs4 import BeautifulSoup
 from googleapiclient.discovery import build
 from spanbert import SpanBERT
@@ -29,12 +29,14 @@ def get_query(api_key, engine_key, query):
 
 def to_plaintext(url):
     print("\tFetching text from url ...")
-    res = requests.get(url)
-    html_page = res.content.decode('utf-8','ignore')
-    soup = BeautifulSoup(html_page, 'html.parser')
-    [s.decompose() for s in soup(['script','sup','i'])]
-    text = soup.body.get_text()
-    # import code; code.interact(local=dict(globals(), **locals()))
+    try:
+        res = requests.get(url)
+        html_page = res.content
+        soup = BeautifulSoup(html_page, 'html.parser')
+        [s.decompose() for s in (soup(['img','script','head','ul','li','ol','nav']))]
+        text = soup.get_text(strip=True, separator=' ').replace("\n", "").replace("\t", "")
+    except:
+        return ""
     print(len(text))
     return text[:20000]
 
@@ -48,12 +50,15 @@ def annotate(text, relationship, threshold):
 
     print(f"\tExtracted {num_sentences} sentences. Processing each sentence one by one to check for presence of right pair of named entity types; if so, will run the second pipeline ...")
     index = 1
-    for sentence in doc.sents:
+    num_extracted = 0
+    num_relations = 0
+    num_overall = 0
+    for sentence in doc.sents:    
+        flag = False
         if((index%5==0)):
             print(f"\tProcessed {index} / {num_sentences} sentences")
-        ents = get_entities(sentence, entities_of_interest)
         candidate_pairs = []
-        sentence_entity_pairs = create_entity_pairs(sentence, entities_of_interest)
+        sentence_entity_pairs = create_entity_pairs(sentence.strip(), entities_of_interest)
         for ep in sentence_entity_pairs:
             # TODO: keep subject-object pairs of the right type for the target relation (e.g., Person:Organization for the "Work_For" relation)
             if((ep[1][1] == target_subject) and (ep[2][1] in target_objects)):
@@ -69,6 +74,7 @@ def annotate(text, relationship, threshold):
     
         relation_preds = spanbert.predict(candidate_pairs)  # get predictions: list of (relation, confidence) pairs
         for ex, pred in list(zip(candidate_pairs, relation_preds)):
+            num_overall +=1
             r = pred[0]
             confidence = pred[1]
             subj = ex["subj"][0]
@@ -78,18 +84,28 @@ def annotate(text, relationship, threshold):
                 print(f"\t\tSentence: {sentence}")
                 print(f"\t\tConfidence: {confidence}; Subject: {subj}; Object: {obj}")
                 if((confidence >= threshold)):
-                    print(f"\t\tAdding to the set of extracted relations")
+                    flag = True
                     key = (subj, obj)
                     if key in extracted_tuples:
-                       extracted_tuples[key] = max(extracted_tuples[key], confidence) 
+                        if(confidence < extracted_tuples[key]):
+                            print("Duplicate with lower confidence than existing record. Ignoring this.")
+                        else:
+                            print(f"\t\tAdding to the set of extracted relations")
+                            num_relations +=1
+                            extracted_tuples[key] = confidence
                     else:
-                       extracted_tuples[key] = confidence
+                        print(f"\t\tAdding to the set of extracted relations")
+                        num_relations +=1
+                        extracted_tuples[key] = confidence
                 else:
                     print(f"\t\tConfidence is lower than threshold confidence. Ignoring this.")
                 print(f"\t\t==========")        
-        
+        if(flag):
+            num_extracted += 1
 
-        index +=1    
+        index +=1
+    print(f"Extracted annotations for  {num_extracted}  out of total  {num_sentences}  sentences")    
+    print(f"Relations extracted from this website: {num_relations} (Overall: {num_overall})")
     return doc
 
 #def spanBert_phase(threshhold, relation, num_tuples,  queried, extracted_tuples, candidate_pairs):
@@ -132,38 +148,51 @@ if __name__ == "__main__":
     iterations = 0
     
     seen_urls = []
-    while(iterations != 1):
+    while((iterations != 1) or (len(extracted_tuples) < num_tuples)):
         print(f"=========== Iteration: {iterations} - Query: {query} ===========")
         results = get_query(client_key,engine_key,query)
+        # results = [ {"link": "https://en.wikipedia.org/wiki/Mark_Zuckerberg"},
+        #             {"link": "https://news.harvard.edu/gazette/story/2017/05/mark-zuckerbergs-speech-as-written-for-harvards-class-of-2017/"},
+        #             {"link": "https://www.youtube.com/watch?v=BmYv8XGl-YU"},
+        #             {"link": "https://www.bbc.com/news/world-us-canada-40053163"},
+        #             {"link": "https://www.youtube.com/watch?v=gsefnhTK5lc"},
+        #             {"link": "https://www.wbur.org/news/2017/05/25/zuckerberg-harvard-commencement"},
+        #             {"link": "https://www.youtube.com/watch?v=zW_0b1Vy-Ts"},
+        #             {"link": "https://www.bostonglobe.com/metro/2018/04/11/mark-zuckerberg-couldn-stop-mentioning-his-harvard-dorm-room-during-facebook-testimony/slW4rCXyIdyctzEuB93J4L/story.html"},
+        #             {"link": "https://www.youtube.com/watch?v=yqr6yLyuHQA"},
+        #             {"link": "https://www.cnbc.com/2019/02/06/mark-zuckerbergs-dad-offered-him-college-or-a-mcdonalds-franchise.html"}]
         num_urls = len(results)
         for i in range(len(results)):
-            url = results[i]['formattedUrl']
+            url = results[i]['link']
             print(f"URL ( {i+1} / {len(results)}): {url}")
             if(url not in seen_urls):
                 seen_urls.append(url)
                 text = to_plaintext(url)
+                if(len(text) <= 0):
+                    continue
                 print(f"\tWebpage length (num characters): {len(text)}")
                 annotated = annotate(text,relation, threshold)
-        
-        print(seen_urls)
-        
+            else:
+                print(f"URL already seen. skipping...")
+                
         sorted_extracted_tuples = sorted(extracted_tuples.items(), key=lambda item: item[1], reverse=True)
+        print(f"============= ALL RELATIONS for {possible_relations[relation][0]} ({len(extracted_tuples)}) =============")
+        for res in sorted_extracted_tuples:
+            print(f"Confidence: {res[1]}          | Subject: {res[0][0]}       | Object: {res[0][1]}")
+   
         if(len(extracted_tuples) < num_tuples):
-            new_query = ""
+            old_query = query
             for entry in sorted_extracted_tuples:
                 temp_query = entry[0][0] + " " + entry[0][1]
                 if temp_query not in queried:
-                    new_query = temp_query
+                    query = temp_query
                     break
-            print("This is the new query {}".format(new_query))
-            if (len(new_query) < 1):
+            print("This is the new query {}".format(query))
+            if (old_query == query):
                 print("ISE has stalled before retrieving k high-confidence tuples")
                 break
-        else:
-            print(f"============= ALL RELATIONS for {possible_relations[relation][0]} ({len(extracted_tuples)}) =============")
-            for res in sorted_extracted_tuples:
-                print(f"Confidence: {res[2]}          | Subject: {res[0]}       | Object: {res[1]}")
-            print(f"Total # of iterations = {iterations}")
-            break
                           
-        iterations+=1   
+        iterations+=1
+    print(f"Total # of iterations = {iterations+1}")
+
+    
