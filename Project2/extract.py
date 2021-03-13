d10659e25894055f933111cb11217440562896b1
jkim10 <jyk2149@columbia.edu>
2021-03-13 15:27:02 -0500

    fixing spaces

--- Project2/extract.py
+++ Project2/extract.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
 import sys
-import urllib
 import requests
 import html
 from bs4 import BeautifulSoup
@@ -28,19 +26,23 @@ def get_query(api_key, engine_key, query):
     ).execute()
     return res['items']
 
+
+# Extract plaintext from url
 def to_plaintext(url):
     print("\tFetching text from url ...")
     try:
-        res = requests.get(url)
-        html_page = html.unescape(res.text)
+        res = requests.get(url,timeout=0.5)
+        html_page = res.text
         soup = BeautifulSoup(html_page, 'html.parser')
-        [s.decompose() for s in (soup(['img','script','head','ul','li','ol','nav']))]
-        text = soup.get_text(strip=True, separator=' ').replace("\n", "").replace("\t", "")
+        [s.decompose() for s in (soup(['img','script','head','ul','li','ol','nav','form']))]
+        text = soup.get_text(strip=True, separator=' ').replace("\n", "").replace("\t", "").replace("  ", ' ')
     except:
         return ""
     print(len(text))
     return text[:20000]
 
+
+# Annotate with Spacy, then extract relations using SpanBert
 def annotate(text, relationship, threshold):
     print("\tAnnotating the webpage using spacy...")
     target_subject = possible_relations[relationship][1]
@@ -73,7 +75,6 @@ def annotate(text, relationship, threshold):
         if len(candidate_pairs) == 0:
             index +=1
             continue
-    
         relation_preds = spanbert.predict(candidate_pairs)  # get predictions: list of (relation, confidence) pairs
         for ex, pred in list(zip(candidate_pairs, relation_preds)):
             r = pred[0]
@@ -110,21 +111,8 @@ def annotate(text, relationship, threshold):
     print(f"Relations extracted from this website: {num_relations} (Overall: {num_overall})")
     return doc
 
-#def spanBert_phase(threshhold, relation, num_tuples,  queried, extracted_tuples, candidate_pairs):
-#    relation_preds = spanbert.predict(candidate_pairs)
-#    for ex, pred in list(zip(candidate_pairs, relation_preds)):
-#        if (pred[0] == possible_relations[relation][0]):
-#            confidence = pred[1]
-#            if (confidence >= threhold):
-#                curr_tuple = (ex["subj"][0], ex["obj"][0]), confidence)
-#                if curr_tuple not in extracted_tuples:
-#                    extracted_tuples.add(curr_tuple)
-#            else:
-#                continue
-    
-           
-
 if __name__ == "__main__":
+
     # First parse and validate arguments
     if(len(sys.argv) != 7):
         print(f"Usage: {sys.argv[0]} <google api key> <google engine id> <r> <t> <q> <k>")
@@ -154,16 +142,6 @@ if __name__ == "__main__":
         print(f"=========== Iteration: {iterations} - Query: {query} ===========")
         queried.add(query)
         results = get_query(client_key,engine_key,query)
-        # results = [ {"link": "https://en.wikipedia.org/wiki/Mark_Zuckerberg"},
-        #             {"link": "https://news.harvard.edu/gazette/story/2017/05/mark-zuckerbergs-speech-as-written-for-harvards-class-of-2017/"},
-        #             {"link": "https://www.youtube.com/watch?v=BmYv8XGl-YU"},
-        #             {"link": "https://www.bbc.com/news/world-us-canada-40053163"},
-        #             {"link": "https://www.youtube.com/watch?v=gsefnhTK5lc"},
-        #             {"link": "https://www.wbur.org/news/2017/05/25/zuckerberg-harvard-commencement"},
-        #             {"link": "https://www.youtube.com/watch?v=zW_0b1Vy-Ts"},
-        #             {"link": "https://www.bostonglobe.com/metro/2018/04/11/mark-zuckerberg-couldn-stop-mentioning-his-harvard-dorm-room-during-facebook-testimony/slW4rCXyIdyctzEuB93J4L/story.html"},
-        #             {"link": "https://www.youtube.com/watch?v=yqr6yLyuHQA"},
-        #             {"link": "https://www.cnbc.com/2019/02/06/mark-zuckerbergs-dad-offered-him-college-or-a-mcdonalds-franchise.html"}]
         num_urls = len(results)
         for i in range(len(results)):
             url = results[i]['link']
@@ -172,6 +150,7 @@ if __name__ == "__main__":
                 seen_urls.append(url)
                 text = to_plaintext(url)
                 if(len(text) <= 0):
+                    print("Unable to fetch text from URL. Continuing")
                     continue
                 print(f"\tWebpage length (num characters): {len(text)}")
                 annotated = annotate(text,relation, threshold)
