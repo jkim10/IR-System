473742fdb7d065582d6442ea6798264e43e373e5
jkim10 <jyk2149@columbia.edu>
2021-03-31 14:24:03 -0400

    CAtegorized Age

--- Project3/extract_associations.py
+++ Project3/extract_associations.py
@@ -1,21 +1,33 @@
 import sys
 import csv
+from collections import defaultdict
 
 if __name__ == "__main__":
     if(len(sys.argv) < 4):
         print(f"Usage: {sys.argv[0]} <integrated_dataset> <min_sup> <min_conf>")
         quit()
     with open(sys.argv[1]) as f:
-        data = [line.split() for line in f]
+        rows = csv.reader(f)
+        data = list(rows)
         data = data[1:]
     min_sup = sys.argv[2]
     min_conf = sys.argv[3]
+    candidate = set()
+    L = defaultdict(int)
+
     # Apriori Algorithm
-    print(len(data))
+    for row in data:
+        for x in row:
+            L[x] +=1 
+    import code; code.interact(local=dict(globals(), **locals()))
 
-    out = open("output.txt","w")
-    out.write(f"==Frequent Itemsets (min_sup={min_sup})\n")
-    # Print Frequent Itemsets
 
-    out.write(f"==High-confidence association rules (min_conf={min_conf})\n")
-    # Print High Confidence association rules
+
+    
+    # Uncomment when we ready to output
+    # out = open("output.txt","w")
+    # out.write(f"==Frequent Itemsets (min_sup={min_sup})\n")
+    # # Print Frequent Itemsets
+
+    # out.write(f"==High-confidence association rules (min_conf={min_conf})\n")
+    # # Print High Confidence association rules
