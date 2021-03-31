a969ce6c58783ecbb406db869546ccd4cccb0f24
jkim10 <jyk2149@columbia.edu>
2021-03-31 13:50:37 -0400

    reading csv fix

--- Project3/extract_associations.py
+++ Project3/extract_associations.py
@@ -5,17 +5,17 @@ if __name__ == "__main__":
     if(len(sys.argv) < 4):
         print(f"Usage: {sys.argv[0]} <integrated_dataset> <min_sup> <min_conf>")
         quit()
-    data = []
-    file = csv.reader(sys.argv[1])
-    for row in file:
-        data.append(row)
+    with open(sys.argv[1]) as f:
+        data = [line.split() for line in f]
+        data = data[1:]
     min_sup = sys.argv[2]
     min_conf = sys.argv[3]
-    # Algorithm
-
+    # Apriori Algorithm
+    print(len(data))
 
     out = open("output.txt","w")
     out.write(f"==Frequent Itemsets (min_sup={min_sup})\n")
     # Print Frequent Itemsets
+
     out.write(f"==High-confidence association rules (min_conf={min_conf})\n")
     # Print High Confidence association rules
