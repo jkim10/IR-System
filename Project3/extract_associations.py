015385063a71cbc75c0bf1d7be47429a2cacda8b
jkim10 <jyk2149@columbia.edu>
2021-03-31 15:13:24 -0400

    L1

--- Project3/extract_associations.py
+++ Project3/extract_associations.py
@@ -1,7 +1,10 @@
 import sys
 import csv
 from collections import defaultdict
-
+import itertools
+def combinations(L,k):
+    L_k = [x for x in L if len(x) == k-1]
+    return set(itertools.combinations(L, k))
 if __name__ == "__main__":
     if(len(sys.argv) < 4):
         print(f"Usage: {sys.argv[0]} <integrated_dataset> <min_sup> <min_conf>")
@@ -10,15 +13,25 @@ if __name__ == "__main__":
         rows = csv.reader(f)
         data = list(rows)
         data = data[1:]
-    min_sup = sys.argv[2]
-    min_conf = sys.argv[3]
+    min_sup = float(sys.argv[2])
+    min_conf = float(sys.argv[3])
     candidate = set()
     L = defaultdict(int)
+    num_rows = len(data)
 
-    # Apriori Algorithm
+    # TODO: Apriori Algorithm
+    # Calculate L1 Itemsets (line 1 of algorithm)
     for row in data:
         for x in row:
-            L[x] +=1 
+            if(x != ''):
+                L[x] +=1
+    for key in list(L.keys()): # Filter out keys not above min_sup
+        s = L[key] / num_rows
+        if(s < min_sup):
+            del L[key]
+
+    
+    # Start Line 2 of Algorithm 
     import code; code.interact(local=dict(globals(), **locals()))
 
 
@@ -27,7 +40,7 @@ if __name__ == "__main__":
     # Uncomment when we ready to output
     # out = open("output.txt","w")
     # out.write(f"==Frequent Itemsets (min_sup={min_sup})\n")
-    # # Print Frequent Itemsets
+    # # TODO: Print Frequent Itemsets
 
     # out.write(f"==High-confidence association rules (min_conf={min_conf})\n")
-    # # Print High Confidence association rules
+    # # TODO: Print High Confidence association rules
